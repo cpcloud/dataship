c6f0995b70dd8343d03836419c59b04863bf7131
Phillip Cloud <cpcloud@gmail.com>
2015-09-12 17:06:55 -0700

    Install junk

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+import versioneer
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name='dataship',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Phillip Cloud',
+    author_email='cpcloud@gmail.com',
+    description='Relationships for your data',
+    license='BSD',
+    keywords='data database',
+    packages=find_packages(),
+    install_requires=read('requirements.txt').strip().split('\n'),
+    long_description=read('README.md'),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: BSD License',
+    ],
+)

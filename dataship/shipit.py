c7ea6001a3b41438666262c8e6464742111e995b
Phillip Cloud <cpcloud@gmail.com>
2015-09-13 08:47:07 -0700

    Support different output formats

--- dataship/shipit.py
+++ dataship/shipit.py
@@ -24,10 +24,10 @@ def shipit(uri, schema=None):
         yield f.name
 
 
-def drawit(dot_filename):
+def drawit(dot_filename, output_format='pdf'):
     with tempfile.NamedTemporaryFile(delete=False) as g:
         subprocess.check_call(
-            ['dot', dot_filename, '-T', 'pdf', '-o', g.name]
+            ['dot', dot_filename, '-T', output_format, '-o', g.name]
         )
         webbrowser.open('file://%s' % os.path.abspath(g.name))
 
@@ -37,13 +37,14 @@ def parse_args():
     p.add_argument('uri', help='SQLAlchemy database uri')
     p.add_argument('-s', '--schema', help='Database schema', default=None,
                    type=str)
+    p.add_argument('-f', '--format', help='Output format', default='pdf')
     return p.parse_args()
 
 
 def main():
     args = parse_args()
     with shipit(args.uri, args.schema) as filename:
-        drawit(filename)
+        drawit(filename, output_format=args.format)
 
 
 if __name__ == '__main__':

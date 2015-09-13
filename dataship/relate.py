eac63b89fe462e14e8fbdf92825c534b1e997111
Phillip Cloud <cpcloud@gmail.com>
2015-09-12 19:41:49 -0700

    Add graph conversion function

--- dataship/relate.py
+++ dataship/relate.py
@@ -4,6 +4,8 @@ from multipledispatch import dispatch
 
 import toolz
 
+import networkx as nx
+
 
 @dispatch(sa.Table)
 def relate(table):
@@ -36,3 +38,52 @@ def relate(engine):
     metadata = sa.MetaData(bind=engine)
     metadata.reflect()
     return relate(metadata)
+
+
+def format_column(table, column):
+    return '%s.%s' % (table, column)
+
+
+def to_graph(ships):
+    graph = nx.DiGraph()
+    for key in ships:
+        # solid red nodes are relations
+        graph.add_node(
+            key,
+            color='red',
+            fillcolor='red',
+            fontcolor='white',
+            style='filled'
+        )
+        primaries = ships[key]['primary']
+        pkeys = []
+        for p in primaries:
+            n = format_column(key, p)
+
+            # blue nodes are key columns
+            graph.add_node(n, color='blue')
+
+            # blue nodes can be used to reach their table
+            graph.add_edge(n, key, color='red')
+            pkeys.append(n)
+
+        # purple edges form the primary key
+        graph.add_edge(key, pkeys[0], color='purple')
+        for prev, nxt in zip(pkeys[:-1], pkeys[1:]):
+            graph.add_edge(prev, nxt, color='purple')
+
+        for fkey in ships[key]['foreign']:
+            (ftable, fcol), = ships[key]['foreign'][fkey].items()
+
+            # table -> foreign key
+            graph.add_edge(key, format_column(key, fkey))
+
+            # foreign key -> parent
+            graph.add_edge(format_column(key, fkey),
+                           format_column(ftable, fcol), style='dashed')
+    return graph
+
+
+def write_graph(graph, path):
+    with open(path, 'w') as f:
+        f.write(nx.to_agraph(graph).to_string())
